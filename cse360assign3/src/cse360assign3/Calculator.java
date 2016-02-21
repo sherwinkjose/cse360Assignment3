bd6c5a82646193cd82452182b4afeae77a03334b
Sherwin Jose <sherwinkjose@yahoo.com>
2016-02-21 15:42:59 -0700

    Initial Version

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,40 @@
+/*
+ * Sherwin Jose
+ * PIN: 415
+ * Description: Calculator that can add, subtract, multiply, divide, and get the total
+*/
+
+package cse360assign3;
+
+public class Calculator {
+
+	private int total; 
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () { // calculates the total 
+		return 0;
+	}
+	
+	public void add (int value) { // adds the value 
+		
+	}
+	
+	public void subtract (int value) { // subtracts the value
+		
+	}
+	
+	public void multiply (int value) { // multiplies the value
+		
+	}
+	
+	public void divide (int value) { // divides the value
+		
+	}
+	
+	public String getHistory () { // gets the history of the calculations
+		return "";
+	}
+}
